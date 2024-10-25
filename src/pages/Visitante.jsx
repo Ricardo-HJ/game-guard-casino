41e9e33b4b3d0bac322f03ef29bd66320bea6e26
DiegoGuerraA00835430 <88684991+DiegoGuerraA00835430@users.noreply.github.com>
2024-10-24 19:46:35 -0600

    Update Visitante.jsx

--- src/pages/Visitante.jsx
+++ src/pages/Visitante.jsx
@@ -22,14 +22,33 @@ export const Visitante =  (props) => {
                     <div className="content-box">
                         <h2>POKER</h2>
                         <button className="column-button">Play Now</button> {/*AGREGAR ONCLICK PARA NAV */}
-                        <img src="https://pngimg.com/uploads/poker/poker_PNG50.png" alt="Image 2" className="image" /> 
+                        <img 
+                            src="https://pngimg.com/uploads/poker/poker_PNG50.png" 
+                            alt="Image 2" 
+                            className="image" 
+                        /> 
                     </div>
                 </div>
                 <div className="column">
                     <div className="content-box">
                         <h2>BLACKJACK</h2>
-                        <button className="column-button">Play Now</button> {/*AGREGAR ONCLICK PARA NAV */}
-                        <img src="https://png.pngtree.com/png-vector/20220812/ourmid/pngtree-blackjack-png-image_6107450.png " alt="Image 2" className="image" />
+                        <button className="column-button">Play Now</button> 
+                        <img 
+                            src="https://png.pngtree.com/png-vector/20220812/ourmid/pngtree-blackjack-png-image_6107450.png " 
+                            alt="Image 2" 
+                            className="image" 
+                        />
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="content-box">
+                        <h2>ROULETTE</h2>
+                        <button className="column-button" onClick={() => navigate('/ruleta')}>Play Now</button> {/*AGREGAR ONCLICK PARA NAV */}
+                        <img
+                            src="https://i.pinimg.com/originals/29/be/02/29be02186cc40532049ad529a7ff11af.png"
+                            alt="Roulette"
+                            className="image"
+                        />
                     </div>
                 </div>
             </div>
